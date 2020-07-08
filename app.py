a4b351ec7c4ab140b7737d0c0f0434a0cb828033
dassaev-lima <dassaevnascimento@gmail.com>
2020-07-08 17:28:35 -0300

    criando lista de tarefas com duas tarefas

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+tarefas = [
+    {
+        'id':0,
+        'tarefa':'Desenvolver uma API flask',
+        'responsavel':'Dassaev',
+        'status':False
+    },
+    {
+        'id':1,
+        'tarefa':'estudar elementor',
+        'responsavel':'dev',
+        'status':False
+    }
+
+]
 @app.route("/",methods=['GET'])
 def tarefa():
     return 'Olá mundo'
